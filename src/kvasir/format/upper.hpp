fe353bc3606d232b1c2d647f5de24eb264fed32a
porkybrain <holmes.odin@gmail.com>
2017-07-21 18:38:10 +0200

    updated mpl submodule

--- src/kvasir/format/upper.hpp
+++ src/kvasir/format/upper.hpp
@@ -1,7 +1,23 @@
 #pragma once
+#include <kvasir/abcd/abcd.hpp>
 
 namespace kvasir {
     namespace format {
+        template<char B, char E, char O>
+        struct char_replace{
+            template<typename>
+                    using f = void; //no public interface
+            constexpr operator()(char c){
+                return (c>=B && c<=E)? c-(B-O):c;
+            }
+        };
 
+        namespace detail{
+            template<typename T>
+            struct pair_range_public{
+
+            };
+        }
+        using pair_range = abcd::wrap_trival_public_interface<detail::pair_range_public>;
     }
 }
--- test/test1.hpp
+++ test/test1.hpp
@@ -1,7 +1,8 @@
-#include "../src/format.hpp"
+#include "../src/kvasir/format/format.hpp"
 
 namespace test1{
     int run() {
+        auto fmt = kvasir::abcd::
         return 0;
     }
 }
