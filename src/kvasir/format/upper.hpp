234ef2c6a73a20575e9e2e34d666f47e3fd8dcbe
Odin Holmes <holmes.odin@gmail.com>
2017-07-24 22:34:00 +0200

    basic upper working

--- src/kvasir/format/upper.hpp
+++ src/kvasir/format/upper.hpp
@@ -1,23 +1,38 @@
 #pragma once
-#include <kvasir/abcd/abcd.hpp>
+#include "../../../abcd/src/kvasir/abcd/abcd.hpp"
+#include <utility>
 
 namespace kvasir {
     namespace format {
         template<char B, char E, char O>
         struct char_replace{
             template<typename>
-                    using f = void; //no public interface
-            constexpr operator()(char c){
+            using f = void; //no public interface
+            constexpr char operator()(char c){
                 return (c>=B && c<=E)? c-(B-O):c;
             }
         };
 
-        namespace detail{
-            template<typename T>
-            struct pair_range_public{
+		using to_upper_EN = char_replace<'a', 'z', 'A'>;
 
+        namespace detail{
+			struct make_upper{};
+            template<typename T>
+            struct pair_char_range_public{
+				std::pair<char*, char*> process(std::pair<char*, char*> in) {
+					using namespace kvasir::abcd;
+					auto it = in.first;
+					for (; it < in.second; it++) {
+						for_each(access<T>(this), capability_t<make_upper>{}, [&](auto& a) {*it = a(*it); });
+					}
+					return { it,in.second };
+				}
             };
         }
-        using pair_range = abcd::wrap_trival_public_interface<detail::pair_range_public>;
+        using pair_char_range = ::kvasir::abcd::wrap_trivial_public_interface<detail::pair_char_range_public>;
     }
+	namespace abcd {
+		template<char B, char E, char D>
+		struct has_capability<format::char_replace<B, E, D>, format::detail::make_upper> :std::true_type {};
+	}
 }
